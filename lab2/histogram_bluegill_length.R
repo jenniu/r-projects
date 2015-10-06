1052e049ae64ce82fde3c40c2509f2d31170f734
Jenni Edwards <jenniferlynnedwards@hotmail.com>
2015-10-06 16:17:24 -0700

    fixed histogram

--- lab2/histogram_bluegill_length.R
+++ lab2/histogram_bluegill_length.R
@@ -1,4 +1,8 @@
 bluegill = read.table(file="lab2.csv", header="true", sep=",")
 attach(bluegill)
 
-hist(Length, main="", xlab="Length (mm)", ylab="Number of individuals", col="gray")
+hist(Length, main="", xlab="Length (mm)", ylab="Number of individuals", col="gray", xaxt = "n", yaxt = "n")
+axis(1, at = seq(0, 250, 50))
+axis(2, at = seq(0, 250, 50))
+
+# ! http://stackoverflow.com/q/32981156/5411676
